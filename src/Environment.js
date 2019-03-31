4642305737757fbb9f44b57585300521429789ec
Tuan Anh Vu <vutuananh@ginno.com>
2019-03-31 12:01:12 +0700

    Fixed api url

--- src/Environment.js
+++ src/Environment.js
@@ -1,8 +1,9 @@
 import { create } from 'apisauce'
 
 const api = create({
-    baseURL: "http://10.1.110.33:9090",
-    // baseURL: "http://localhost:9090",
+    //baseURL: "http://10.1.110.33:9090",
+    //baseURL: "http://localhost:9090",
+    baseURL: "http://18.136.201.129:9090",
     headers: { Accept: "application/json" }
 });
 
