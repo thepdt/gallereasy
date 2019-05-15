070748d0109e3a999bce824dd07a57335f314809
thep <thephpvn@gmail.com>
2019-05-15 10:28:03 +0700

    FIX baseUrl

--- src/Environment.js
+++ src/Environment.js
@@ -7,8 +7,8 @@ const apiManage = create({
 });
 
 const apiStatistic = create({
-    baseURL: "http://localhost:9091/crawl/v1.0",
-    // baseURL: "http://18.136.201.129:9091/crawl/v1.0",
+    // baseURL: "http://localhost:9091/crawl/v1.0",
+    baseURL: "http://18.136.201.129:9091/crawl/v1.0",
     headers: { Accept: "application/json" }
 });
 
